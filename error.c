0a11adf714482283b2a59e0b7aaab9e969b50745
DianeDerycke <dianedhayat@gmail.com>
2018-06-14 17:01:48 +0200

    fix gestion error error_option

--- error.c
+++ error.c
@@ -6,26 +6,24 @@
 /*   By: DERYCKE <DERYCKE@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 15:15:14 by DERYCKE           #+#    #+#             */
-/*   Updated: 2018/06/14 15:59:27 by DERYCKE          ###   ########.fr       */
+/*   Updated: 2018/06/14 16:58:32 by DERYCKE          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_ls.h"
 
 void	error_option(char c)
 {
-	extern int 	errno;
 
-	ft_putstr("ls: illegal option -- ");
-	ft_putchar(c);
-	ft_putchar('\n');
-	ft_putstr("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]\n");
-	strerror((errno));
+	ft_putstr_fd("ls: illegal option -- ", 2);
+	ft_putchar_fd(c, 2);
+	ft_putchar_fd('\n', 2);
+	ft_putstr_fd("usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]\n", 2);
 	exit(1);
 }
 
 void	error_no_file_or_dir(char *str)
 {
-	ft_putstr("ls: ");
-	ft_putstr(str);
-	ft_putstr(": No such file or directory\n");
+	ft_putstr_fd("ls: ", 2);
+	ft_putstr_fd(str, 2);
+	ft_putstr_fd(": No such file or directory\n", 2);
 }
